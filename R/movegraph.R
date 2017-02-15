a2981456fb980041de756f957b7173d68c2ed55a
rpatin <remi.patin@free.fr>
2017-02-15 09:26:16 +0100

    First Commit

---
+++ R/movegraph.R
@@ -0,0 +1,8 @@
+#' movegraph: tools for network analysis of animal GPS movement data
+#'
+#' movegraph provides usefull tools for network analysis of animal movement
+#'
+#' @docType package
+#' @name movegraph
+
+NULL
